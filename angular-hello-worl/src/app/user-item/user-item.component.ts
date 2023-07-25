fa1267b4f2b6fb7db1d3cafa7553f16a7dc8dc5c
Filip Swierczynski <swirek.gw@hotmail.com>
2023-07-25 12:38:05 -0400

    first app completed

---
+++ angular-hello-worl/src/app/user-item/user-item.component.ts
@@ -0,0 +1,14 @@
+import { Component,Input } from '@angular/core';
+
+@Component({
+  selector: 'app-user-item',
+  templateUrl: './user-item.component.html',
+  styleUrls: ['./user-item.component.scss']
+})
+export class UserItemComponent {
+  @Input() name: string;
+
+  constructor() {
+   
+  }
+}
